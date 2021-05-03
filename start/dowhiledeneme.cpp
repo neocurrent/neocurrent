2f3a6862841d8f84f797b246fa7f96de3bd9371a
neo current <83499888+neocurrent@users.noreply.github.com>
2021-05-03 21:33:02 +0300

    Update dowhiledeneme.cpp

--- start/dowhiledeneme.cpp
+++ start/dowhiledeneme.cpp
@@ -4,7 +4,7 @@ int main(){
 	int sum=0,n,count=0;
 	float avg;
 	cout<<"  this loop just remember for do-while loops."<<endl;
-	cout<<"  now we want you enter a number for n and you see what happen"<<endl;
+	cout<<"  we want you enter a number for n and then you ll see what happen"<<endl;
 	do{
 		cin>>n;
 		if(n>=0){
@@ -13,7 +13,7 @@ int main(){
 			count++;
 			cout<<"  count = "<<count<<endl;
 			if((count>4)&(count<6))
-			cout<<"  okay man you see if your n number is different from negatives this loop goes infinity :)"<<endl;
+			cout<<"  okay man you see, if your n number is different from negatives this loop goes infinity :)"<<endl;
 			
 		}
 	}
